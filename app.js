4b6faa22ad1da6b4fc7caef028ae9e2317e93af4
tomasz.wojciechowski <tomasz.wojciechowski@cyclad.pl>
2019-09-22 20:52:07 +0200

    17

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-Vue.component('greeting', {
-  template: '<p>reausable components {{ name }} . <button v-on:click="changeName">Change Name</button></p>',
-  data: function() {
-    return {
-      name: 'Tomek',
-    }
+var one = new Vue({
+  el: '#vue-app',
+  data: {
+    output: 'your fav food',
   },
   methods: {
-    changeName: function(){
-      this.name = 'Mario';
+    readRefs: function() {
+      this.output = this.$refs.input.value;
     }
   }
 });
-
-
-var one = new Vue({
-  el: '#vue-app-one',
-
-});
-
-var two = new Vue({
-  el: '#vue-app-two',
-
-});
-
-two.title = "changed from outside";

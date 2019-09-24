d3f753219395dab35f06547bdd0dafbe915cd759
TomaszWu <tomasz.wojciechowski84@gmail.com>
2019-09-24 20:41:35 +0200

    24

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,47 @@
 <template>
-  <div id="app">
-    <app-header></app-header>
-    <ninjas v-bind:ninjas="ninjas"></ninjas>
-    <app-footer></app-footer>
-  </div>
+    <div>
+        <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
+        <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
+        <app-footer v-bind:title="title"></app-footer>
+    </div>
 </template>
 
 <script>
+// Imports
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
 import Ninjas from './components/Ninjas.vue';
 
-
 export default {
-  components: {
-    'app-header': Header,
-    'app-footer': Footer,
-    'ninjas': Ninjas,
-  },
-  data() {
-    return {
-      ninjas: [
-        {name: 'Ryu', speciality: 'Vue Components', show: false},
-        {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-        {name: 'Hitoshi', speciality: 'Click Events', show: false},
-        {name: 'Tango', speciality: 'Conditionals', show: false},
-        {name: 'Kami', speciality: 'Webpack', show: false},
-        {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-      ]
+    components: {
+        'app-header': Header,
+        'app-footer': Footer,
+        'app-ninjas': Ninjas
+    },
+    data () {
+        return {
+          ninjas: [
+              {name: 'Ryu', speciality: 'Vue Components', show: false},
+              {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+              {name: 'Hitoshi', speciality: 'Click Events', show: false},
+              {name: 'Tango', speciality: 'Conditionals', show: false},
+              {name: 'Kami', speciality: 'Webpack', show: false},
+              {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+          ],
+          title: 'Vue Wizards'
+        }
+    },
+    methods: {
+      updateTitle: function(updatedTitle){
+        this.title = updatedTitle;
+      }
     }
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,29 +1,34 @@
 <template>
-  <header>
-    <h1>{{ title }}</h1>
-  </header>
+    <header>
+        <h1 v-on:click="changeTitle">{{ title }}</h1>
+    </header>
 </template>
-
 <script>
-
 export default {
-  data() {
-    return {
-      title: 'Vue Ninjas',
+    props: {
+      title: {
+        type: String,
+        required: true
+      }
+    },
+    data(){
+        return{
+        }
+    },
+    methods: {
+      changeTitle: function(){
+        this.$emit('changeTitle', 'Vue Ninjas 2');
+      }
     }
-  },
-  methods: {
-  }
 }
 </script>
-
 <style scoped>
 header{
-  background: lightgreen;
-  padding: 10px;
+    background: lightgreen;
+    padding: 10px;
 }
 h1{
-  color: #222;
-  text-align: center;
+    color: #222;
+    text-align: center;
 }
 </style>

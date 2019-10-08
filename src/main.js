267e03a29100750c8428e12768e8f5666871209f
TomaszWu <tomasz.wojciechowski84@gmail.com>
2019-10-08 21:22:34 +0200

    35

--- src/main.js
+++ src/main.js
@@ -12,20 +12,13 @@ Vue.directive('rainbow', {
   }
 });
 
-Vue.directive('theme', {
-  bind(el, binding, vnode){
-    if (binding.value == 'wide') {
-      el.style.maxWidth = "1200px"
-    } else if (binding.value == 'narrow') {
-      el.style.maxWidth = "560px"
-    }
+Vue.filter('to-uppercase', function(value) {
+  return value.toUpperCase();
+})
 
-    if (binding.arg == 'column') {
-      el.style.backgroud = red;
-      el.style.padding = '20px'
-    }
-  }
-});
+Vue.filter('snippet', function(value) {
+  return value.slice(0, 100) + '...';
+})
 
 export const bus = new Vue();
 
